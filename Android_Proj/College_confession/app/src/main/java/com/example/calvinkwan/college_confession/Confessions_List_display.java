97401c508d5f8b0decad391e9e32d62f7fe7be9f
Calvin Kwan <calhkwan@gmail.com>
2016-04-20 19:47:47 -0700

    working on deprecated page viewer, need to change to material design

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
@@ -1,15 +1,21 @@
 package com.example.calvinkwan.college_confession;
 
+import android.support.v4.app.FragmentActivity;
+import android.app.ActionBar
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Confessions_List_display extends AppCompatActivity
+public class Confessions_List_display extends FragmentActivity
 {
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confessions__list_display);
+
+        actionBar = getActionBar();
+        actionBar.
     }
 }
---
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/FragmentA.java
@@ -0,0 +1,32 @@
+package com.example.calvinkwan.college_confession;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentA extends Fragment
+{
+
+
+    public FragmentA()
+    {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragmentA_, container, false);
+    }
+
+}
