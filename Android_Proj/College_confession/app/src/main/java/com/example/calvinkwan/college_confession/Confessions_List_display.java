85e4ffae662effffa4f5960c96bde37a7b441f1d
hkwan003 <calvinhkwan@hotmail.com>
2016-04-22 14:03:42 -0700

    added a lot of supprot libraries and got the tab motion moving, looks ugly AF but will fix later

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
@@ -1,12 +1,29 @@
 package com.example.calvinkwan.college_confession;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.app.ActionBar
+import android.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class Confessions_List_display extends FragmentActivity
+import com.example.calvinkwan.college_confession.tabs.SlidingTabLayout;
+
+public class Confessions_List_display extends AppCompatActivity
 {
+    private Toolbar toolbar;
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
+
     ActionBar actionBar;
 
     @Override
@@ -16,5 +33,68 @@ public class Confessions_List_display extends FragmentActivity
         setContentView(R.layout.activity_confessions__list_display);
 
         actionBar = getActionBar();
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setViewPager(mPager);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter
+    {
+
+        int icons[] = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+        String[] tabs = {"tab 1", "tab 2", "tab 3"};
+
+        public MyPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position)
+        {
+            MyFragment myFragment = MyFragment.getInstance(position);
+
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount()
+        {
+            return 3;
+        }
+    }
+
+    public static class MyFragment extends Fragment
+    {
+        private TextView textView;
+        public static MyFragment getInstance(int position)
+        {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+        {
+            View layout = inflater.inflate(R.layout.fragment_a, container, false);
+            textView = (TextView) layout.findViewById(R.id.position);
+            Bundle bundle = getArguments();
+            if(bundle != null)
+            {
+                textView.setText("The page selected is" + bundle.getInt("position"));
+            }
+
+            return layout;
+        }
     }
 }
--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/FragmentA.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/FragmentA.java
@@ -26,7 +26,7 @@ public class FragmentA extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmentA_, container, false);
+        return inflater.inflate(R.layout.fragment_a, container, false);
     }
 
 }
