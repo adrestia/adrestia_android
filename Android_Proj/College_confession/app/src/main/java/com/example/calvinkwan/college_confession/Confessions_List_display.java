8248cfbd19a663682e84cd50ab417e2590c46503
hkwan003 <calvinhkwan@hotmail.com>
2016-05-10 00:15:16 -0700

    added new button instead of floating app button

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/Confessions_List_display.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -37,7 +39,6 @@ public class Confessions_List_display extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confessions__list_display);
-
         actionBar = getActionBar();
 
         mPager = (ViewPager) findViewById(R.id.pager);
@@ -82,6 +83,30 @@ public class Confessions_List_display extends AppCompatActivity
         {
             return 3;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public static class MyFragment extends Fragment
