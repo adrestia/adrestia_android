bc409798b1507615f0bd9f79179a6676ca259e9d
Alex Tan <atan009@ucr.edu>
2016-05-13 18:16:28 -0700

    drop menu on old code

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/MainActivity.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.calvinkwan.college_confession;
 
 import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void Login(View view)
--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/RegisterActivity.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/RegisterActivity.java
@@ -1,36 +1,31 @@
 package com.example.calvinkwan.college_confession;
 
 import android.app.DownloadManager;
-import android.app.ProgressDialog;
 import android.content.ContentValues;
-import android.os.AsyncTask;
 import android.preference.PreferenceActivity;
-import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
-
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
+/*
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+*/
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,8 +34,6 @@ public class RegisterActivity extends AppCompatActivity
 {
     EditText mEmail, mPassword, mConfirmPassword;
     String password1, password2, email;
-    TextView responseText;
-
 
 
 
@@ -55,65 +48,80 @@ public class RegisterActivity extends AppCompatActivity
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         mConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
-        responseText = (TextView) findViewById(R.id.textView6);
+
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+
+
+        String[] items = new String[]{"1", "2", "three"};
+
+        //
+        Vector<String>str=new Vector<String>();
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(getAssets().open("world_universities_and_domains.json.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String line = null;
+        try {
+            line = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int index = 0;
+        while (line != null) {
+            if(line.indexOf("name") != -1)
+                //String college_name = line.substring(8,line.length()-2);
+                str.add(line.substring(15,line.length()-2));
+                try {
+                    line = in.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+        }
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, str);
+
+        spinner.setAdapter(adapter);
+        //
+        /*
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        */
+
     }
 
-    public Map<String, String> ConfirmInfo(View view)
+    public void ConfirmInfo(View view)
     {
         password1 = mPassword.getText().toString();
         password2 = mConfirmPassword.getText().toString();
         email = mEmail.getText().toString();
-
         if(validEmail(email))
         {
-            if (matchingPasswords(password1, password2))
-            {
-                RequestQueue queue = Volley.newRequestQueue(this);
-                String url ="http://www.google.com";
-
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response)
-                    {
-                        // Display the first 500 characters of the response string.
-                        responseText.setText("Response is: "+ response.substring(0,500));
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
-                        responseText.setText("That didn't work!");
-                    }
-                }) {
-
-                    @Override
-                    protected Map<String, String> getParams()
-                    {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("email", email);
-                        params.put("password", password1);
-                        //todo for college ID (Alex Tan)
-                        return params;
-                    }
-                };
-
-// Add the request to the RequestQueue.
-            queue.add(stringRequest);
 
 
         }
-        else {
+        else
+        {
             Toast.makeText(getApplicationContext(),
                     "Please enter a valid edu email", Toast.LENGTH_LONG).show();
         }
-
+        if((password1.equals(password2)))
+        {
+            //future toast message notification
+            Toast.makeText(getApplicationContext(),
+                    "Your have entered a valid password", Toast.LENGTH_LONG).show();
+        }
+        {
+            //future toast message notification
+            Toast.makeText(getApplicationContext(),
+                    password1 + "    " + password2, Toast.LENGTH_LONG).show();
         }
     }
-
 
     public static boolean validEmail(String email)
     {
@@ -136,15 +144,7 @@ public class RegisterActivity extends AppCompatActivity
         //"http://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext"
     }
 
-    public boolean matchingPasswords(String pw1, String pw2)
-    {
-        if(pw1.equals(pw2))
-        {
-            return true;
-        }
-        Toast.makeText(getApplicationContext(),
-                "Your have entered a invalid password" +password1 + "    " + password2, Toast.LENGTH_LONG).show();
-        return false;
-    }
+
+
 
 }
