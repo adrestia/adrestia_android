870fc74be55075792b6d20788470a3b5a3774d9e
Alex Tan <atan009@ucr.edu>
2016-05-17 19:47:21 -0700

    post longer moves submit button

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/PostConfession.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/PostConfession.java
@@ -20,7 +20,6 @@ public class PostConfession extends AppCompatActivity
 
         }
 
-        
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count)
         {
