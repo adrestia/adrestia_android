ffe6fa04c543de3bdc5e8cf107e2e17b263b8f01
Alex Tan <atan009@ucr.edu>
2016-05-18 22:30:24 -0700

    can now post from Android?

--- Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/PostConfession.java
+++ Android_Proj/College_confession/app/src/main/java/com/example/calvinkwan/college_confession/PostConfession.java
@@ -1,16 +1,32 @@
 package com.example.calvinkwan.college_confession;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostConfession extends AppCompatActivity
 {
     private EditText PostConfession;
-    private TextView charCount;
+    private TextView charCount, sResponse;
 
     private final TextWatcher PostCharCounter = new TextWatcher()
     {
@@ -48,5 +64,77 @@ public class PostConfession extends AppCompatActivity
 
 
     }
+
+    public void PostConfession(View view)
+    {
+        sResponse = (TextView) findViewById(R.id.sResponse);
+
+        SharedPreferences pref = getSharedPreferences("API_key", MODE_PRIVATE);
+        String savedAPIKEY = pref.getString("api_KEY", null);
+
+        if(savedAPIKEY != null)
+        {
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "https://dev.collegeconfessions.party/api/posts?apikey=" + savedAPIKEY;
+
+            // Request a string response from the provided URL.
+            final StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
+            {
+                @Override
+                public void onResponse(String response)
+                {
+                    sResponse.setText(response);
+                    try
+                    {
+                        JSONObject responseOBJ = new JSONObject(response);
+
+                        String status = responseOBJ.getString("status");
+                        String apiKey = responseOBJ.getString("api_key");
+                        //Toast.makeText(getApplicationContext(), apiKey, Toast.LENGTH_LONG).show();
+
+                        //Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+
+                    }
+                    catch (Exception e)
+                    {
+
+
+                    }
+                }
+            },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                            sResponse.setText("Please make sure your credentials are valid");
+                        }
+                    })
+
+            {;
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("body", PostConfession.getText().toString());
+                    return params;
+                }
+
+                //https://gist.github.com/mombrea/7250835
+
+            };
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+
+
+        }
+
+
+
+    }
+
 
 }
